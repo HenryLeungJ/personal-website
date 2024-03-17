dcb0c5673341a9c7ee1cb8b281a51529766a2e1b
HenryLeungJ <leunghenry619@gmail.com>
2024-03-16 19:14:00 -0700

    yes

--- src/components/me.tsx
+++ src/components/me.tsx
@@ -24,7 +24,7 @@ export default function Henry() {
               <h2 className="text-base font-semibold leading-7 text-indigo-600">Software Engineer</h2>
               <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Henry Leung</p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                A <b>software engineer</b>.<br />Combine Major in <b>Business and Computer Science</b> at the <b>University of British Columbia</b>.<br />Always open to new experiences
+                A <b>software engineer</b>.<br />Combined Major in <b>Business and Computer Science</b> at the <b>University of British Columbia</b>.<br />Always open to new experiences
                 and learning opportunities.
               </p>
               <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
